4f4031bd88ca5c9ad6bca2abd435109b92cd072f
Brendan Reed <b.reed@setonhill.edu>
2020-02-04 20:57:35 -0500

    Initial Commit

---
+++ Brendan Swift Sample 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brendan Swift Sample 1
+//
+//  Created by Brendan Reed on 2/4/20.
+//  Copyright © 2020 Brendan Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
